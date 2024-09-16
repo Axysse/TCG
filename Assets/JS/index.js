7e8df0b23e52686c14a22cc84a137f8205de00d9
Axysse <nicolas.vouillerot@gmail.com>
2024-09-16 16:23:56 +0200

    init

---
+++ Assets/JS/index.js
@@ -0,0 +1,226 @@
+let all = [];
+document.addEventListener('DOMContentLoaded', function () {
+    let PVPNJ = 20
+    let PVJC = 20
+    let pvpnj = document.getElementById("pvpnj")
+    let pvjc = document.getElementById("pvjc")
+    pvpnj.innerText = PVPNJ
+    pvjc. innerText = PVJC
+    fetch("https://raw.githubusercontent.com/the-fab-cube/flesh-and-blood-cards/develop/json/english/card.json")
+        .then(res => res.json())
+        .then(data => {
+            for(let i = 0; i < data.length; i++){
+                all.push({ id: data[i].unique_id, name: data[i].name, pitch: data[i].pitch, cost: data[i].cost, power: data[i].power, defense :data[i].defense, health: data[i].health, intelligence: data[i].intelligence, types: data[i].types, text: data[i].functional_text, types: data[i].type_text, image: data[i].printings[0]["image_url"]} )
+            }
+            console.log(all)
+
+
+function getRandomId(){
+    return randomIndex = Math.floor(Math.random() * all.length);
+}
+
+zone1 = document.getElementById("zone1")
+zone2 = document.getElementById("zone2")
+
+for( let i = 0; i <= 3; i++) {
+    let id = getRandomId()
+    let img = document.createElement("img")
+    img.src= all[id]["image"]
+    img.setAttribute("id", "adversaire"+[i])
+    zone1.appendChild(img).value = all[id]["id"];
+}
+
+for( let i = 0; i <= 3; i++) {
+    let id = getRandomId()
+    let img = document.createElement("img")
+    img.src= all[id]["image"]
+    img.setAttribute("id", "main"+[i])
+    zone2.appendChild(img).value = all[id]["id"]
+    // let stats = [[all[id]["id"]], [all[id]["cost"]], [all[id]["defense"]], [all[id]["health"]], [all[id]["power"]]]
+    // console.log(stats)
+}
+
+let main = zone2.getElementsByTagName('img')
+console.log(main)
+
+let adversaire = zone1.getElementsByTagName('img')
+console.log(adversaire)
+
+main0 = document.getElementById("main0")
+main1 = document.getElementById("main1")
+main2 = document.getElementById("main2")
+main3 = document.getElementById("main3")
+
+// Appel de la carte 0
+main0.addEventListener("click", () => {
+
+    let atq = null;
+    let def = null;
+
+    function getRandomCard(){
+        return randomIndex = Math.floor(Math.random() * adversaire.length);
+    } 
+
+    let defId = getRandomCard()
+
+    all.forEach(el => {
+        if(el['id'] == adversaire[defId].value){
+            def = el['defense']
+            console.log(def)
+            return def
+        }
+    })
+    
+    all.forEach(element => {
+        if(element['id'] == main0.value){
+            atq = element['power']
+            console.log(atq)
+            return atq;
+        }
+    })
+
+    let dmg = atq - def
+    console.log(dmg)
+
+    let zone3 = document.getElementById("zone3")
+    let h2 = document.createElement("h2")
+    if(dmg <= 0){
+        h2.innerText = "Vous avez infligé 0 de dégats"
+    } else {
+    h2.innerText = "Vous avez infligé "+ dmg + " de dégats" 
+    zone3.appendChild(h2)
+    pvpnj.innerText = PVPNJ - dmg }
+})
+
+// Appel de la carte 1
+main1.addEventListener("click", () => {
+
+    let atq = null;
+    let def = null;
+
+    function getRandomCard(){
+        return randomIndex = Math.floor(Math.random() * adversaire.length);
+    } 
+
+    let defId = getRandomCard()
+
+    all.forEach(el => {
+        if(el['id'] == adversaire[defId].value){
+            def = el['defense']
+            console.log(def)
+            return def
+        }
+    })
+    
+    all.forEach(element => {
+        if(element['id'] == main1.value){
+            atq = element['power']
+            console.log(atq)
+            return atq;
+        }
+        
+    })
+
+    let dmg = atq - def
+    console.log(dmg)
+
+    let zone3 = document.getElementById("zone3")
+    let h2 = document.createElement("h2")
+    if(dmg <0){
+        h2.innerText = "Vous avez infligé 0 de dégats"
+    } else {
+    h2.innerText = "Vous avez infligé "+ dmg + " de dégats" 
+    zone3.appendChild(h2)
+
+        pvpnj.innerText = PVPNJ - dmg}
+})
+
+// Appel de la carte 2
+main2.addEventListener("click", () => {
+
+    let atq = null;
+    let def = null;
+
+    function getRandomCard(){
+        return randomIndex = Math.floor(Math.random() * adversaire.length);
+    } 
+
+    let defId = getRandomCard()
+
+    all.forEach(el => {
+        if(el['id'] == adversaire[defId].value){
+            def = el['defense']
+            console.log(def)
+            return def
+        }
+    })
+    
+    all.forEach(element => {
+        if(element['id'] == main2.value){
+            atq = element['power']
+            console.log(atq)
+            return atq;
+        }
+        
+    })
+
+    let dmg = atq - def
+    console.log(dmg)
+
+    let zone3 = document.getElementById("zone3")
+    let h2 = document.createElement("h2")
+    if(dmg <0){
+        h2.innerText = "Vous avez infligé 0 de dégats"
+    } else {
+    h2.innerText = "Vous avez infligé "+ dmg + " de dégats" 
+    zone3.appendChild(h2)
+
+        pvpnj.innerText = PVPNJ - dmg}
+})
+
+// Appel de la carte 3
+main3.addEventListener("click", () => {
+
+    let atq = null;
+    let def = null;
+
+    function getRandomCard(){
+        return randomIndex = Math.floor(Math.random() * adversaire.length);
+    } 
+
+    let defId = getRandomCard()
+
+    all.forEach(el => {
+        if(el['id'] == adversaire[defId].value){
+            def = el['defense']
+            console.log(def)
+            return def
+        }
+    })
+    
+    all.forEach(element => {
+        if(element['id'] == main3.value){
+            atq = element['power']
+            console.log(atq)
+            return atq;
+        }
+        
+    })
+
+    let dmg = atq - def
+    console.log(dmg)
+
+    let zone3 = document.getElementById("zone3")
+    let h2 = document.createElement("h2")
+    if(dmg <0){
+        h2.innerText = "Vous avez infligé 0 de dégats"
+    } else {
+    h2.innerText = "Vous avez infligé "+ dmg + " de dégats" 
+    zone3.appendChild(h2)
+    pvpnj.innerText = PVPNJ - dmg}
+})
+                    
+})
+})
+
+
---
+++ Readme.md
@@ -0,0 +1,74 @@
+﻿# **TCG**
+[![forthebadge](https://forthebadge.com/images/featured/featured-built-with-love.svg)](https://forthebadge.com)
+## A PROPOS
+
+Site réalisé dans le cadre d'un TD durant la formation développeur WEB à l'AFPA de Châtellerault.
+
+# Table des matières
+
+ - [A propos](##Apropos)
+ - [Prérequis](#Prérequis)
+ - [Contribution](#Contribution)
+ - [Documentation](#Documentation)
+ - [Licence](#Licence)
+
+
+# Prérequis
+
+Les ressources sont disponibles plus haut sur la page GitHub du projet.
+
+# Contributions
+
+Un grand merci à:
+
+ - L'API Flesh and blood
+
+
+# Logiciels/Langages/Librairies
+
+ - Visual Studio Code
+ - HTML/CSS
+ - Javascript
+ - Tailwind CSS
+
+# Documentation
+
+ - [Maquette](##Maquette) 
+ - [Index](##Index)
+
+## Maquette
+
+![Maquette](https://ibb.co/BwTNnKQ)
+
+La maquette originelle, plutôt spartiate, est dans l'ensemble identique au produit final, si ce n'est le changement de couleurs pour rendre moins terne le produit fini et qui correspond mieux à la DA du jeu originel.
+
+## Index
+La page contient un header et un footer classiques sans aucune spécificités. Le centre de la page est découpé en deux zones de jeu. La zone supérieure représentant la main de l’adversaire (non cliquable du coup) et la zone inférieure représente la main du joueur. Les 4 cartes du joueur sont cliquables mais n'ont d'effet que si la carte a une valeur d'attaque.
+
+**Le responsive**
+
+Le site a été codé en responsive-first. Son affichage par défaut est donc en colonne. Mais dès que la taille de l'écran dépasse une valeur le site passe en affichage horizontal, plus adapté aux écrans d'ordinateur.
+
+![responsive](https://ibb.co/bRZcXQ0)
+
+**Le javascript**
+
+On fetch d'abord l'API pour récupérer toutes les cartes du jeu et on stocke dans un tableau les données qui nous intéresse: l'id, l'attaque, la défense, l'image, ect..
+Puis on distribue 4 cartes au hasard au joueur et à son adversaire chacun dans leur zone de jeu. Pour cela on crée des éléments images qu'on ajoute aux zones de jeu, ces images, côté joueur, on pour valeur l'id de la carte, comme ça on peut facilement retrouver les informations nécessaires au jeu en fonction du choix de la carte. 
+
+Les 4 cartes de la main sont cliquables mais n'ont d'effet que si on est sûr de faire des dégâts à l'adversaire qui, lui, va se défendre avec une carte prise aléatoirement de sa main. Ce qui veut dire que, parfois, il va se défendre avec sa seule carte qui n'a pas de valeur de défense.
+Les PV de l'adversaire baissent en conséquence et un texte affiche le nombre de dégâts infligés.
+
+
+
+# Licence
+
+Tout droits reservés Nicolas Vouillerot
+
+Linkedin: https://www.linkedin.com/in/nicolas-vouillerot-a62402287/
+
+CoursDuDev: https://www.coursdudev.com/profil/Axysse
+
+GitHub: https://github.com/Axysse
+
+> Written with [StackEdit](https://stackedit.io/).
